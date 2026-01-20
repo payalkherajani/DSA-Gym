00b41796df63f6f4b5685a6f25a8fb23a45ebe76
Payal Kherajani <payal.kherajani19@gmail.com>
2026-01-20 12:20:18 +0530

    chore: bubble sort algorithm

---
+++ Sorting/BubbleSort.js
@@ -0,0 +1,19 @@
+//Complexity of this solution is O(N^2) which is not good at all
+function BubbleSort(arr) {
+    const n = arr.length
+    for (let i = 0; i < n-1; i++){
+      for (let j = 0; j < n-1-i; j++) {
+        if(arr[j] > arr[j+1]) {
+            //swap
+             let temp = arr[j]
+              arr[j] = arr[j+1]
+              arr[j+1] = temp
+            
+        }
+       
+      }
+    }
+    return arr
+}
+
+console.log(BubbleSort([5,2,4,1])) 
