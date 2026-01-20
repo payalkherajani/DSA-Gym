5fe5ad2aa23ae781d42bc550bed555c2bac0c371
Payal Kherajani <payal.kherajani19@gmail.com>
2026-01-20 12:28:33 +0530

    chore: bubble sort algorithm improved version

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -17,3 +17,26 @@ function BubbleSort(arr) {
 }
 
 console.log(BubbleSort([5,2,4,1])) 
+
+//Improvment - If no swapping is happening in sorting algorithm i.e j loop so we can break the loop
+
+ //Complexity of this solution is O(N^2) which is not good at all
+function BubbleSortImproved(arr) {
+    const n = arr.length
+    for (let i = 0; i < n-1; i++){
+         let isSwapped = false
+      for (let j = 0; j < n-1-i; j++) {
+        if(arr[j] > arr[j+1]) {
+            //swap
+             let temp = arr[j]
+              arr[j] = arr[j+1]
+              arr[j+1] = temp   
+              isSwapped = true
+        }       
+      }
+        if(!isSwapped) break
+    }
+    return arr
+}
+
+console.log(BubbleSortImproved([1,2,3,4])) 
