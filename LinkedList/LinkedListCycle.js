a7a18f22178b0ea637096ad3a17dd280a99df38f
Payal Kherajani <payal.kherajani19@gmail.com>
2026-02-05 11:47:15 +0530

    chore: floyd algorithm for cycle linked list

--- LinkedList/LinkedListCycle.js
+++ LinkedList/LinkedListCycle.js
@@ -1,3 +1,5 @@
+//Approach-1
+
 var hasCycle = function(head) {
    let current = head 
    let hasCycle = false
@@ -12,4 +14,22 @@ var hasCycle = function(head) {
 
    }
    return hasCycle
+};
+
+
+//Approach-2 (Flyod Approach)
+var hasCycle = function(head) {
+   if(!head) return false
+   let slowPointer = head
+   let fastPointer = head.next
+
+   while(slowPointer !== fastPointer) {
+    if(fastPointer === null || fastPointer.next === null) {
+        return false
+    }
+    slowPointer = slowPointer.next
+    fastPointer = fastPointer.next.next
+   }
+
+   return true
 };
